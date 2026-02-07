791877776b0968f0752aedb4dbc883952e58b250
MAURICIOCARDOZ <cmauricardozo74@gmail.com>
2026-02-07 00:11:01 -0300

    07/02 00:10

--- DirtyballFINAL/Assets/Scripts/Armas/Armas Clase.cs
+++ DirtyballFINAL/Assets/Scripts/Armas/Armas Clase.cs
@@ -1,30 +1,24 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class ArmasClase : MonoBehaviour
 {
-    public int rango = 1;
-    public int daño = 1;
-    public float cooldown = 1f;
 
     private Collider colliderArma;
+    public AtributosEnemigoBase scriptEnemigo;
 
 
     private void Start()
     {
         colliderArma = GetComponent<Collider>();
-        if (colliderArma == null)
-        {
-            Debug.LogWarning(gameObject.name + ": No se encontró un Collider en el arma.");
-        }
-        else
-        {
-            colliderArma.isTrigger = true; // Asegura que el collider sea un trigger
-        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("COLISION CON BOTELLA");
+        if (other.CompareTag("Player")) {
+           scriptEnemigo.Atacar(other.gameObject);
+        }
     }
 }
 
